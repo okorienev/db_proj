bdbb46ae68a12b4a3b994f3fb27067fb172ef5d6
Oleksandr Korienev <alexkorienev@gmail.com>
2018-11-30 22:00:09 +0200

    add default logging,, update __init__, add have_permission
    have_permission to check current user permission to use the scenario
    __init__ now adds OperationType object to action class instance
    default logging which writes date, user and operation type

--- app/actions/AbstractAction.py
+++ app/actions/AbstractAction.py
@@ -1,3 +1,8 @@
+from abc import abstractmethod
+from app.models import get_session, User, OperationType, Operation
+import datetime
+
+
 class AbstractAction:
     """
     class to declare common interface for console implementation of use cases
@@ -5,15 +10,32 @@ class AbstractAction:
     ACTION_NAME: str = None
     COMMAND_NAME: str = None
 
-    def __init__(self, user):
-        self.user = user
+    def log_operation(self):
+        session = get_session()
+        operation = Operation(date=datetime.datetime.utcnow(),
+                              user=self.user,
+                              operation_type=self.operation_type)
+        session.add(operation)
+        session.commit()
+
+    def __init__(self, user: User):
+        self.user: User = user
+        session = get_session()
+        self.operation_type: OperationType = session.query(OperationType).filter(
+            OperationType.name==self.ACTION_NAME
+        ).first()
+        if not self.operation_type:
+            raise ValueError("Can't find operation with given type")
 
     def have_permission(self) -> bool:
         """
+        todo universal permission check for all cases
         :return: should return boolean value of current user permission to commit action
         """
-        raise NotImplementedError
+        return self.user.role in self.operation_type.rights.roles
 
+
+    @abstractmethod
     def handle(self) -> None:
         """
         method which gets control over the application (input, output, queries, etc)
